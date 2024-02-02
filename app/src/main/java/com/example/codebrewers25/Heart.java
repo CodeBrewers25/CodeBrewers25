a246232bd16688aba7f93ffbca9baf0bfc7efea1
yashbabariya <107163142+Babariyayash@users.noreply.github.com>
2024-02-02 13:59:19 -0700

    Update Heart.java

--- app/src/main/java/com/example/codebrewers25/Heart.java
+++ app/src/main/java/com/example/codebrewers25/Heart.java
@@ -1,8 +1,8 @@
 package com.example.codebrewers25;
 
 public abstract class Heart extends Shape {
+    String color = "red";
     private int x;
     private int y;
-    String color = "blue";
     public Heart();
 }
